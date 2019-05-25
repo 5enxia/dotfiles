01809ba22633b8132b2b073020bbf8b8ea7d3d28
takayasu_morishita <c011729632@edu.teu.ac.jp>
2019-05-25 22:57:57 +0900

    add .bash_profile, .bashrc, .gitconfig, .gitignore, .vim, .zplug, .zshrc, setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+rm ~/.bashrc
+
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zplug ~/.zplug
+
+
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
