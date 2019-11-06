1e76df4fa78b9eac1ac431b2c2fd26882644c5ff
takayasu_morishita <c011729632@edu.teu.ac.jp>
2019-11-06 16:22:09 +0900

    modified fish/config.fish on macos

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,3 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH $HOME/.local/share/omf
-
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG $HOME/.config/omf
-
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 # fish theme
 set fish_theme agnoster
 
