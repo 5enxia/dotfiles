fd711a52a3c3e84f567403da7006fb1d2bdc3ac7
takayasu_morishita <c011729632@edu.teu.ac.jp>
2019-11-06 16:19:09 +0900

    modified fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,31 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH $HOME/.local/share/omf
+
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG $HOME/.config/omf
+
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
 # fish theme
 set fish_theme agnoster
 
 
-# ls/cd 
+# ls/cd
 alias c='cd ../'
 function mkcd
     mkdir -p $argv; cd $argv;
 end
 
-alias sl='ls -CF'	
-alias ll='ls -alF'	
-alias la='ls -A'	
-alias l='ls -CF'	
+alias sl='ls -CF'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
 
-alias dir='dir --color=auto'	
-alias vdir='vdir --color=auto'	
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
 
 
 # powerline-shell
