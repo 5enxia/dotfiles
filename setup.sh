a6b68b084e40ceaa4b217188d82aaa14b1a5b265
takayasu_morishita <c011729632@edu.teu.ac.jp>
2019-11-06 14:19:01 +0900

    add s-link fish/

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # shell
-rm ~/.bashrc
+# rm ~/.bashrc
 # ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 # ln -sf ~/dotfiles/.bashrc ~/.bashrc
 # ln -sf ~/dotfiles/.zshrc ~/.zshrc
 # ln -sf ~/dotfiles/.zplug ~/.zplug
-ln -sf ~/dotfiles/.config/fish  ~/.config/fish
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 
 # vim/neovim
 ln -sf ~/dotfiles/.vim ~/.vim
@@ -14,7 +14,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 # others
 ## slate
-if [['uname' == 'Darwin']]; then
+if [ "$(uname)" == 'Darwin' ]; then
     ln -sf ~/dotfiles/.slate.js ~/.slate.js
 fi
 
